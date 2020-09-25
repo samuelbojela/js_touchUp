65e3e9affc9c09574812593729d4290c5a8d1e17
Samuel Bojela <bojela@gmail.com>
2020-09-25 12:46:47 -0400

    creating variables

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert('This is an external script test');
+var y = 3;
+let z = 'Zebra'; //Watch your syntax!!
+
+const WEEK = 7;
+
+if (y == 3) {
+  let z = 'Mantis Shrimp age: ';
+  element = document.getElementById('p1');
+  element.innerHTML = z;
+  element = document.getElementById('p2');
+  element.innerHTML = y;
+}
+
+console.log(z);
+console.log(y);
